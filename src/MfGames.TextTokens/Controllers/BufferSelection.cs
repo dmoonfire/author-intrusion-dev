348ced5ae8c441bfe4895c2c787b1965ced8f63f
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2014-07-12 00:04:27 -0500

    Added the beginning of a selection.

---
+++ src/MfGames.TextTokens/Controllers/BufferSelection.cs
@@ -0,0 +1,74 @@
+﻿// <copyright file="BufferSelection.cs" company="Moonfire Games">
+//     Copyright (c) Moonfire Games. Some Rights Reserved.
+// </copyright>
+// MIT Licensed (http://opensource.org/licenses/MIT)
+namespace MfGames.TextTokens.Controllers
+{
+    using MfGames.TextTokens.Buffers;
+    using MfGames.TextTokens.Texts;
+
+    /// <summary>
+    /// Encapsulates the functionality for cursors and selections within a buffer view. This
+    /// is used by the UserBufferController to determine where the selection is and also for
+    /// functionality that involves replacing the contents with input.
+    /// </summary>
+    public class BufferSelection
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BufferSelection"/> class.
+        /// </summary>
+        /// <param name="buffer">
+        /// The buffer.
+        /// </param>
+        public BufferSelection(IBuffer buffer)
+        {
+            this.Buffer = buffer;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets the anchor of the selection. An anchor is where the selection starts.
+        /// </summary>
+        /// <value>
+        /// The anchor.
+        /// </value>
+        public TextLocation? Anchor { get; private set; }
+
+        /// <summary>
+        /// Gets the buffer associated with the selection.
+        /// </summary>
+        /// <value>
+        /// The buffer.
+        /// </value>
+        public IBuffer Buffer { get; private set; }
+
+        /// <summary>
+        /// Gets the cursor location within the buffer.
+        /// </summary>
+        /// <value>
+        /// The cursor.
+        /// </value>
+        public TextLocation Cursor { get; private set; }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// Sets the cursor and clears the anchor.
+        /// </summary>
+        /// <param name="textLocation">The text location.</param>
+        public void SetCursor(TextLocation textLocation)
+        {
+            this.Cursor = textLocation;
+            this.Anchor = null;
+        }
+
+        #endregion
+    }
+}
--- src/MfGames.TextTokens/Controllers/UserBufferController.cs
+++ src/MfGames.TextTokens/Controllers/UserBufferController.cs
@@ -30,6 +30,7 @@ namespace MfGames.TextTokens.Controllers
         public UserBufferController(IBuffer buffer)
         {
             this.Buffer = buffer;
+            this.Selection = new BufferSelection(buffer);
         }
 
         #endregion
@@ -46,10 +47,22 @@ namespace MfGames.TextTokens.Controllers
 
         #endregion
 
+        #region Properties
+
+        /// <summary>
+        /// Gets the selection associated with the controller.
+        /// </summary>
+        /// <value>
+        /// The selection.
+        /// </value>
+        protected BufferSelection Selection { get; private set; }
+
+        #endregion
+
         #region Public Methods and Operators
 
         /// <summary>
-        /// Inserts text into a given location, chosing the appropriate token to modify
+        /// Inserts text into a given location, choosing the appropriate token to modify
         /// and passing it into the buffer as an undoable command.
         /// </summary>
         /// <param name="textLocation">
@@ -60,37 +73,50 @@ namespace MfGames.TextTokens.Controllers
         /// </param>
         public void InsertText(TextLocation textLocation, string text)
         {
+            this.SetCursor(textLocation);
+            this.InsertText(text);
+        }
+
+        /// <summary>
+        /// Inserts text at the cursor position, choosing the appropriate token to modify
+        /// and passing it into the buffer as an undoble command.
+        /// </summary>
+        /// <param name="text">
+        /// The text to insert.
+        /// </param>
+        public void InsertText(string text)
+        {
             // Establish our contracts.
             Contract.Requires(text != null);
 
+            // We'll be populating the lines.
+            var command = new BufferCommand();
+
             // Get the token at this point in the buffer.
+            TextLocation cursor = this.Selection.Cursor;
             IToken oldToken = this.Buffer.GetToken(
-                textLocation.LineIndex, textLocation.TokenIndex);
+                cursor.LineIndex, cursor.TokenIndex);
 
             // Figure out the new text of the string and create a new token with the modified
             // version. This will also copy the attributes of the old token.
-            string newText = oldToken.Text.Insert(
-                textLocation.TextIndex.Index, text);
-
+            string newText = oldToken.Text.Insert(cursor.TextIndex.Index, text);
             IToken newToken = this.Buffer.NewToken(oldToken, newText);
 
             // Create a buffer command, add the replacement operation, and then
             // submit it to the buffer.
             const int SingleTokenReplacement = 1;
-            var command = new BufferCommand
-                {
-                    new ReplaceTokenOperation(
-                        textLocation.LineIndex, 
-                        textLocation.TokenIndex, 
-                        SingleTokenReplacement, 
-                        newToken)
-                };
+            command.Add(
+                new ReplaceTokenOperation(
+                    cursor.LineIndex, 
+                    cursor.TokenIndex, 
+                    SingleTokenReplacement, 
+                    newToken));
 
             this.Buffer.Do(command);
         }
 
         /// <summary>
-        /// Rexecutes the last reverses command on the buffer, or do nothing if there are no
+        /// Re-executes the last reversed command on the buffer, or do nothing if there are no
         /// commands that can be redone.
         /// </summary>
         public void Redo()
@@ -99,8 +125,19 @@ namespace MfGames.TextTokens.Controllers
         }
 
         /// <summary>
-        /// Reverses the last command on the buffer or nothing if there are no reversable
-        /// commands.
+        /// Sets the cursor location in the selection.
+        /// </summary>
+        /// <param name="textLocation">
+        /// The text location.
+        /// </param>
+        public void SetCursor(TextLocation textLocation)
+        {
+            this.Selection.SetCursor(textLocation);
+        }
+
+        /// <summary>
+        /// Reverses the last command on the buffer or nothing if there are no commands
+        /// that can be reversed.
         /// </summary>
         public void Undo()
         {
