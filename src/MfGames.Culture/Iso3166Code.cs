b1d82aca18e9ac288d22ca7bd1bf2b0826187900
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2014-07-22 13:38:35 -0500

    Adding in the beginnings of MfGames.Culture.

---
+++ src/MfGames.Culture/Iso3166Code.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="Iso3166Code.cs" company="Moonfire Games">
+//     Copyright (c) Moonfire Games. Some Rights Reserved.
+// </copyright>
+// MIT Licensed (http://opensource.org/licenses/MIT)
+namespace MfGames.Culture
+{
+    /// <summary>
+    /// An immutable identifier for an ISO 3166 country code including support for 
+    /// unknown countries.
+    /// </summary>
+    public class Iso3166Code
+    {
+    }
+}
---
+++ src/MfGames.Culture/Iso639Manager.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="Iso639Manager.cs" company="Moonfire Games">
+//     Copyright (c) Moonfire Games. Some Rights Reserved.
+// </copyright>
+// MIT Licensed (http://opensource.org/licenses/MIT)
+namespace MfGames.Culture
+{
+    /// <summary>
+    /// A manager class which ensures only a single instance of a <c>Iso639Code</c>
+    /// is created for a given language.
+    /// </summary>
+    public class Iso639Manager
+    {
+    }
+}
